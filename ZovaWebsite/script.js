600e1fcb3b964027c185f5a778fbc4a206b65df6
Carlo Milosavljevic <63962935+CarloMilos@users.noreply.github.com>
2024-12-12 12:55:25 +0100

    Add files via upload

--- ZovaWebsite/script.js
+++ ZovaWebsite/script.js
@@ -7,5 +7,29 @@
             navbar.classList.remove("frosted-glass");
         }
     });
+document.addEventListener('DOMContentLoaded', function() {
+  const scribble = document.querySelector('.nectar-scribble');
+  const footer = document.querySelector('.footer'); // Select the correct footer element
 
-    
+  let hasAnimated = false;
+
+  function handleIntersection(entries) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && !hasAnimated) {
+        scribble.classList.add('animate');
+        hasAnimated = true;
+      }
+    });
+  }
+
+  const observer = new IntersectionObserver(handleIntersection);
+  observer.observe(footer);
+});
+
+function fadeInHero() {
+    const heroContainer = document.querySelector('.hero-container');
+    heroContainer.classList.add('fade-in');
+  }
+  
+  // Call the function on page load
+  window.addEventListener('load', fadeInHero);
